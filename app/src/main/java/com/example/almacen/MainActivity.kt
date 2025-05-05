b5547c451babcf95cd24112dd27ace3223031098
Guslin-03 <david.comeronal@elorrieta-errekamari.com>
2025-05-05 16:18:30 +0200

    Primeras Pantallas

--- app/src/main/java/com/example/almacen/MainActivity.kt
+++ app/src/main/java/com/example/almacen/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.almacen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
+    private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //espera y muestra login
+        handler.postDelayed({ logIn() }, 3000)
+    }
+
+    private fun logIn() {
+        val intent = Intent(this, LogInActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
